1902f977c47ce8ed955064fb30376699a21d6153
pRom3o <alameleromeo1@gmail.com>
2026-01-13 10:26:54 +0100

    apply

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -3,32 +3,32 @@ import NavBar from '@/components/NavBar.vue'
 
 import { onMounted, watch } from 'vue'
 import { RouterView } from 'vue-router'
-import { useToggleStore } from '@/store/toggle'
-import ThemeToggle from '@/components/themeToggle.vue'
+// import { useToggleStore } from '@/store/toggle'
+// import ThemeToggle from '@/components/themeToggle.vue'
 
-const toggleStore = useToggleStore()
+// const toggleStore = useToggleStore()
 
-// Watch the theme and apply dark class
-watch(
-  () => toggleStore.currentTheme,
-  (newTheme) => {
-    document.documentElement.classList.toggle('dark', newTheme === 'dark')
-    localStorage.setItem('theme', newTheme)
-  },
-)
+// // Watch the theme and apply dark class
+// watch(
+//   () => toggleStore.currentTheme,
+//   (newTheme) => {
+//     document.documentElement.classList.toggle('dark', newTheme === 'dark')
+//     localStorage.setItem('theme', newTheme)
+//   },
+// )
 
-// Load saved theme on mount
-onMounted(() => {
-  const saved = localStorage.getItem('theme')
-  if (saved) toggleStore.currentTheme = saved
-  document.documentElement.classList.toggle('dark', toggleStore.currentTheme === 'dark')
-})
+// // Load saved theme on mount
+// onMounted(() => {
+//   const saved = localStorage.getItem('theme')
+//   if (saved) toggleStore.currentTheme = saved
+//   document.documentElement.classList.toggle('dark', toggleStore.currentTheme === 'dark')
+// })
 </script>
 
 <template>
   <NavBar />
   <main class="relative bg-white transition-colors duration-300 min-h-screen">
     <RouterView />
-    <ThemeToggle class="fixed sm:bottom-4 sm:right-6 bottom-2 right-2" />
+    <!-- <ThemeToggle class="fixed sm:bottom-4 sm:right-6 bottom-2 right-2" /> -->
   </main>
 </template>
